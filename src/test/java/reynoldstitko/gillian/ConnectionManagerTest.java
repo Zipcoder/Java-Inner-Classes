1db54e45e700b32532bcff06cc60e0602e641f56
Gillian Reynolds-Titko <gillianreynolds-titko@zipcoders-MacBook-Pro.local>
2017-02-02 20:03:43 -0500

    Stubbed methods

--- src/test/java/reynoldstitko/gillian/ConnectionManagerTest.java
+++ src/test/java/reynoldstitko/gillian/ConnectionManagerTest.java
@@ -3,12 +3,16 @@ package reynoldstitko.gillian;
 import org.junit.Before;
 import org.junit.Test;
 
+
+import java.util.ArrayList;
+
 /**
  * Created by gillianreynolds-titko on 2/2/17.
  */
 public class ConnectionManagerTest {
 
     ConnectionManager connectionManager;
+    //ConnectionManager.ManagedConnection managedConnection = connectionManager.new ManagedConnection();
 
     @Before
     public void setUp(){
@@ -29,7 +33,10 @@ public class ConnectionManagerTest {
 
     @Test
     public void maxNumberOfConnectionsReached(){
-        //This method wil return null if the connection limit is reached
+        ArrayList<Connection> connections = new ArrayList<>(4);
+        //connections.add()
+        //This method will return null if the connection limit is reached
+
     }
-    //------
+
 }
